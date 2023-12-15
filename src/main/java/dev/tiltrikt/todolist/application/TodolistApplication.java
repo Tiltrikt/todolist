28f4b5aec6bfbfce8a57e37d510bca39af6653ad
Tiltrikt <vladoss339@gmail.com>
2023-12-15 22:21:09 +0100

    testing Controller

--- src/main/java/dev/tiltrikt/todolist/application/TodolistApplication.java
+++ src/main/java/dev/tiltrikt/todolist/application/TodolistApplication.java
@@ -2,12 +2,10 @@ package dev.tiltrikt.todolist.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication(scanBasePackages = "dev.tiltrikt.todolist")
-@EnableJpaRepositories(basePackages = "dev.tiltrikt.todolist.repository")
-@EntityScan(basePackages = "dev.tiltrikt.todolist.model")
+@Configuration
 public class TodolistApplication {
 
   /* TODO: 08.12.2023
---
+++ src/test/java/dev/tiltrikt/todolist/application/TodolistApplicationTest.java
@@ -0,0 +1,13 @@
+package dev.tiltrikt.todolist.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = TodolistApplication.class)
+class TodolistApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
